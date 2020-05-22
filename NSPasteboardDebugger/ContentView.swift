c5517c6f2f42b8ac4182b36ef3c5c6d1eadce604
Jakob Egger <jakob@eggerapps.at>
2020-05-22 16:34:10 +0200

    Initial Commit

---
+++ NSPasteboardDebugger/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NSPasteboardDebugger
+//
+//  Created by Jakob Egger on 2020-05-22.
+//  Copyright © 2020 Egger Apps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
